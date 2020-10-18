e15a9862990881c150ad5b5bfd53771446d69cc4
peng00bo00 <pengbo_tongji@126.com>
2020-10-18 13:33:31 +0400

    update CanvasData.h

--- homeworks/project/src/hw1/Components/CanvasData.h
+++ homeworks/project/src/hw1/Components/CanvasData.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <UGM/UGM.h>
+#include <Eigen/Core>
 
 struct CanvasData {
 	std::vector<Ubpa::pointf2> points;
@@ -8,6 +9,19 @@ struct CanvasData {
 	bool opt_enable_grid{ true };
 	bool opt_enable_context_menu{ true };
 	bool adding_line{ false };
+
+	bool lagrange_interpolation{ false };
+	bool rbf_interpolation{ false };
+	bool linear_regression{ false };
+	bool ridge_regression{ false };
+
+	Eigen::VectorXf w_rbf;
+	Eigen::VectorXf w_lr;
+	Eigen::VectorXf w_rr;
+
+	int n_order = 3;
+	float sigma = 100;
+	float lambda = 0;
 };
 
 #include "details/CanvasData_AutoRefl.inl"
